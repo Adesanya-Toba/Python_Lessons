cd3d5384ddb3eec47f38048fcd450d030a973f21
Toba Adesanya <adesanyatoba@gmail.com>
2023-03-27 09:23:08 +0100

    importing packages

--- importing_packages.py
+++ importing_packages.py
@@ -1,6 +1,10 @@
 import HelloWorld.Fncs_wReturns as f
+from Py3_OOP.classes import Dog
 
 # square = HelloWorld.Fncs_wReturns.square()
+new_Dog = Dog("Vix", 7)
 val = 5
 print(f.squareroot(val))
 print(f.square(val))
+
+print (new_Dog)
