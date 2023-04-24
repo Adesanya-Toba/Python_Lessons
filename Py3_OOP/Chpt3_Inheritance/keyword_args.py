c73f6aa09a25bf7e6b397ad986ace5e62c5a3764
Toba Adesanya <adesanyatoba@gmail.com>
2023-04-24 11:12:08 +0100

    Using **kwargs

--- Py3_OOP/Chpt3_Inheritance/keyword_args.py
+++ Py3_OOP/Chpt3_Inheritance/keyword_args.py
@@ -5,10 +5,11 @@ class Contact:
         super().__init__(**kwargs)
         self.name = name
         self.email = email
+        # self.phone = phone
         self.all_contacts.append(self)
 
 class AddressHolder:
-    def __init__(self, street='', city='', state='', code='',
+    def __init__(self, street='', city='', state='', code='', phone='',
                  **kwargs) -> None:
         super().__init__(**kwargs)
         self.street = street
@@ -17,11 +18,28 @@ class AddressHolder:
         self.code = code
 
 class Friend(Contact, AddressHolder):
-    def __init__(self, phone='', **kwargs):
+    def __init__(self, **kwargs):
+        # if we want 'phone' to be available in the kwargs dictionary,
+        # we could update kwargs before calling super.
+        # kwargs.update(dict(phonez=phone))
+        # OR
+        # kwargs.update({'phone': phone})
+        # print(kwargs)
         super().__init__(**kwargs)
+        self.phone = kwargs['phone']
+        print(self.phone)
+
+    def assign(self,age, **kwargs):
+        self.age = age
+        self.my_name = kwargs['name']
+        self.payload = kwargs['payload']
+        print(self.payload)
 
 def main():
-    Friend(phone='901', name= 'Toba', email= 'toba@you.com')
+    AFriend = Friend(name='Toba', email= 'Toba@you.com', phone= 901)
+    print(AFriend)
+
+    AFriend.assign(27, name= AFriend.name, payload= 'Hello, how are you')
 
 
 if __name__ == '__main__':
