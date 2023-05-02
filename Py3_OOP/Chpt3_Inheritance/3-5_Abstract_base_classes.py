2c455a2b571bcb3242858ffc9542e76563760407
Toba Adesanya <adesanyatoba@gmail.com>
2023-05-02 11:48:43 +0100

    Creating my own abstract class

--- Py3_OOP/Chpt3_Inheritance/3-5_Abstract_base_classes.py
+++ Py3_OOP/Chpt3_Inheritance/3-5_Abstract_base_classes.py
@@ -20,3 +20,59 @@ print(isinstance(odd_container, Container))
 print(issubclass(OddContainer, Container))
 
 print('Using the "in" keyword: ', 1 in odd_container)
+
+# Create an Abstract Base Class
+import abc
+
+class MediaLoader(metaclass=abc.ABCMeta):
+    @abc.abstractmethod # Python decorator. Because it's marked as abstract, this means that any subclass must implement this method
+    def play(self):
+        pass
+
+    @abc.abstractproperty
+    def ext(self):
+        pass
+
+    @classmethod # Meaning this method can be called on a Class instead of an instatiated object.
+    def __subclasshook__(cls, C):
+        '''This says that any class that supplies the concrete implementations of
+        all the attributes of this ABC should be considered a subclass of 
+        MediaLoader, even if it doesn't actually inherit from MediaLoader.
+        '''
+        if cls is MediaLoader:
+            attrs = set(dir(C)) # Get a set of methods and props in this class including any parent classes and hierarchy
+            if set(cls.__abstractmethods__) <= attrs:
+                '''Using set notation to check if the methods have been implemented in this class'''
+                return True
+            
+        return NotImplemented
+
+# Testing these new functions and constructs
+attrs = set(dir(MediaLoader))
+print(attrs)
+print('\n')
+print(set(MediaLoader.__abstractmethods__))
+
+class Wav(MediaLoader):
+        pass
+
+class Ogg(Wav):
+    ext = '.ogg'
+
+    def play(self):
+        pass
+
+class DecoratorTest():
+
+    @classmethod
+    def decor_print(cls):
+        print("Printing directly from class DecoratorTest")
+
+    def another_func(self):
+        print('Printing from a non-class method.')
+
+# x = Wav() # Will not work because the Wav class doesn't implement play() or ext
+y = Ogg()
+
+DecoratorTest.decor_print()
+# DecoratorTest.another_func() # Does not work.
--- detect_os.py
+++ detect_os.py
@@ -9,3 +9,28 @@ if return_string.find("?") != -1:
     return_string += "&"
     print(return_string.find('@'))
     print('here')
+
+separator = '/'
+key = 'RTK_V1/Z123456789ABCDEF/Python-Auto-Client/DataLog/1682618798/chunk/integrity'
+key_list = key.split(separator)
+key_list.pop()
+print(key_list)
+
+combined_str = separator.join(key_list) + separator
+print(combined_str)
+
+before, sep, after = key.partition('/')
+
+print(before, sep, after)
+new_key = key.removesuffix('integrity')
+print(new_key)
+
+# Creating a new type in python
+from typing import Union
+from typing_extensions import TypeAlias
+
+special_var:TypeAlias = Union [str, int, None]
+special_var2: TypeAlias = str | int | None # requires python 3.10
+
+_sp_str: special_var = 'hello'
+print(_sp_str.upper())
