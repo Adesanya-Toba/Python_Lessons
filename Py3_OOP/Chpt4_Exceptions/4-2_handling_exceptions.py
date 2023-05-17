e9f09e759e5dfacf1143e21435e9374d1ff549b3
Toba Adesanya <adesanyatoba@gmail.com>
2023-05-17 11:28:30 +0100

    multi-layer exception handling

--- Py3_OOP/Chpt4_Exceptions/4-2_handling_exceptions.py
+++ Py3_OOP/Chpt4_Exceptions/4-2_handling_exceptions.py
@@ -24,8 +24,8 @@ def funniest_division(divisor:int) -> Union[str, float]:
     except TypeError:
         return 'Enter a numerical value'
     except ValueError:
-        print('No, No, not 13!')
-        raise
+        return('No, No, not 13!')
+        # raise
 
 def never_returns() -> NoReturn:
     print("I am about to raise an exception")
@@ -45,3 +45,29 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
+    try:
+        raise ValueError('This is an argument!', 400)
+    except ValueError as e:
+        print(f'The exception arguments were: {e.args}') # prints with trailing comma
+        print(f'The exception arguments were: {e!r}') # prints entire exception line -> ValueError('This.., 400')
+
+    some_exceptions = [ValueError, TypeError, IndexError, None]
+
+    for choice in some_exceptions:
+        try:
+            print(f'\nRaising {choice}')
+            if choice:
+                raise choice('An error')
+            else:
+                print('no exception raised')
+        except ValueError:
+            print('Caught a ValueError')
+        except TypeError:
+            print('Caught a TypeError')
+        except Exception as e:
+            print(f'Caught some other error: {e.__class__.__name__}')
+        else:
+            print('This code is called if there are no exceptions')
+        finally:
+            print('This cleanup code is always called.')
