2c6837063d171b7009801b983a40efe22af09e06
Toba <adesanyatoba@gmail.com>
2023-06-13 09:43:41 +0100

    5-1 using OOP - making points

--- Py3_OOP/Chpt5_UsingOOP/5-1_using_oop.py
+++ Py3_OOP/Chpt5_UsingOOP/5-1_using_oop.py
@@ -1,5 +1,5 @@
 from math import hypot
-from typing import Tuple, List, Optional, Iterable
+from typing import Tuple, List, Optional, Iterable, Union
 
 class Point:
     def __init__(self, x: float, y: float) -> None:
@@ -27,16 +27,37 @@ class Polygon2:
         self.vertices = list(vertices) if vertices else []
 
     def perimeter(self) -> float:
+        # print(self.vertices)
+        # print(self.vertices[1:]+ self.vertices[:1])
         pairs = zip(self.vertices, self.vertices[1:] + self.vertices[:1])
         return sum(p1.distance(p2) for p1, p2 in pairs)
+
+
+Pair = Tuple[float, float]
+Point_or_Tuple = Union[Point, Pair]
+
+class Polygon3:
+    def __init__(self, vertices: Optional[Iterable[Point_or_Tuple]]= None) -> None:
+        self.vertices: List[Point] = []
+        if vertices:
+            for point_or_tuple in vertices:
+                self.vertices.append(self.make_point(point_or_tuple))
+
+    @staticmethod
+    def make_point(item: Point_or_Tuple) -> Point:
+        return item if isinstance(item, Point) else Point(*item)
 
 
 if __name__ == '__main__':
     var1 = (1, 2, 3)
     var2 = ('a', 'b', 'c', 'd', 'e')
 
-    print(var2[2:]) # print everything from the 2th element
-    print(var2[:3]) # print the first 3 items in the list
+    # print(var2[2:]) # print everything from the 2th element
+    # print(var2[:3]) # print the first 3 items in the list
 
     result = zip(var1, var2)
-    print(set(result))
+    # print(p3, p2 in result)
+    # print(set(result))
+
+    square = Polygon2([Point(1,1), Point(1,2), Point(2,2), Point(2,1)])
+    print(square.perimeter())
