ebd4d2a745c941665094fe3fa2d2aa2de599ee59
Toba <adesanyatoba@gmail.com>
2023-12-21 10:13:04 +0000

    OOP attributes - properties in detail

--- Py3_OOP/Chpt5_UsingOOP/5-2_attributes.py
+++ Py3_OOP/Chpt5_UsingOOP/5-2_attributes.py
@@ -11,6 +11,7 @@ class Color:
         self._name = name
 
     def _get_name(self) -> str:
+        """Get color name"""
         return self._name
 
     def set_rgb_value(self, rgb_value: int):
@@ -26,6 +27,34 @@ class Color:
     name = property(_get_name, _set_name)
 
 
+class NorwegianBlue:
+    def __init__(self, name) -> None:
+        self._name = name
+        self._state: str
+
+    def test_method(self):
+        """Test method"""
+        pass
+
+    def _get_state(self):
+        """This is the get method for class NorwegianBlue.
+        If no docString is provided in the property description,
+        the docstring in the getter method is used.
+        """
+        print(f"Getting {self._name}'s state")
+        return self._state
+
+    def _set_state(self, state: str):
+        print(f"Setting {self._name}'s state to {state!r}")
+        self._state = state
+
+    def _del_state(self):
+        print(f"{self._name} is pushing up daisies!")
+        del self._state
+
+    silly = property(_get_state, _set_state, _del_state)
+
+
 def main():
     c = Color(0xFF0000, "bright red")
     print(c.name)
@@ -34,8 +63,19 @@ def main():
 
     # Setting attributes directly using properties
     c.name = "Black"
-    # c.name = ""  # Raises a ValueError
+    try:
+        c.name = ""  # Raises a ValueError because this operation goes through the
+    except ValueError:
+        print("Expected value error.")
+    # property (_set_name) setter function which performs input validations.
     print(c.name)  # Works as well
+
+    p = NorwegianBlue("Polly")
+    p.silly = "Pinning for the fjords"
+    print(p.silly)
+    del p.silly
+
+    print("hello")
 
 
 if __name__ == "__main__":
